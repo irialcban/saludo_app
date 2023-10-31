0d4ed4e99faa4998c64e89d5e4536fe56c711f88
Iria <irialcban@alu.edu.gva.es>
2023-10-31 13:09:06 +0100

    Text creado Variable vinculada al botón en el MainActivity

--- app/src/main/java/com/pmdm/saludoapp/MainActivity.kt
+++ app/src/main/java/com/pmdm/saludoapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.pmdm.saludoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnHello: AppCompatButton = findViewById(R.id.btnHello)
+        btnHello.setOnClickListener {
+            Log.i("Test","Botón pulsado")
+        }
     }
 }
