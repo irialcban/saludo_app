3e8c7f34e062904e121c0ca6fb67c6954d50d6bd
Iria <irialcban@alu.edu.gva.es>
2023-11-03 13:01:49 +0100

    Toast hecho

--- app/src/main/java/com/pmdm/saludoapp/MainActivity.kt
+++ app/src/main/java/com/pmdm/saludoapp/MainActivity.kt
@@ -3,16 +3,21 @@ package com.pmdm.saludoapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val btnHello: AppCompatButton = findViewById(R.id.btnHello)
+        val etName: AppCompatEditText = findViewById(R.id.etName)
+
         btnHello.setOnClickListener {
-            Log.i("Test","Botón pulsado")
+            //Log.i("Test","Botón pulsado")
+            val name = etName.text.toString()
+            Toast.makeText(this,"Hola $name!",Toast.LENGTH_LONG).show()
         }
     }
 }
