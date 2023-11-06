4bde92385a5207cda8feff5b7960c55c53ab5cd1
Iria <irialcban@alu.edu.gva.es>
2023-11-06 11:22:55 +0100

    Snackbar hecho

--- app/src/main/java/com/pmdm/saludoapp/MainActivity.kt
+++ app/src/main/java/com/pmdm/saludoapp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatEditText
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +18,10 @@ class MainActivity : AppCompatActivity() {
         btnHello.setOnClickListener {
             //Log.i("Test","Botón pulsado")
             val name = etName.text.toString()
-            Toast.makeText(this,"Hola $name!",Toast.LENGTH_LONG).show()
+            //Toast.makeText(this,"Hola $name!",Toast.LENGTH_LONG).show()
+            if(name.isNotEmpty()) {
+                Snackbar.make(btnHello,"Hola $name!",Snackbar.LENGTH_SHORT).setAnchorView(btnHello).setAction("CLOSE"){ finishAffinity()}.show()
+            }
         }
     }
 }
